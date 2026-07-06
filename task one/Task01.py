a3b2881f6b5d9a47c3c582c3341b6e8fffa05c34
QasimNaveed-stack <qsimnaveed303@gmail.com>
2026-07-06 11:45:18 +0500

    initial commit

---
+++ task one/Task01.py
@@ -0,0 +1,39 @@
+# #Task 01 Trafic Lights Code
+# print ("Traffic lights code : ")
+# color = input ("Enter the color of traffic light : ") 
+# if (color == "red"):
+#     print ("Stop")
+# elif (color == "yellow"):
+#     print ("Get ready")
+# elif (color == "green"):
+#     print ("Go")
+# else:
+#     print ("Broken traffic light")    
+#------------------------------------------
+# Type casting
+# a = "10" # type string
+# b = 20.25 # type float
+# # sum = a + b # TypeError: can only concatenate str (not "float") to str
+# c = float(a)
+# sum = c + b # Type casting string to float
+# print(sum)
+#----------------------------------
+# Program to take two inputs from the user and print thier sum
+# print ("Sum of two numbers : ")
+# a = int (input ("Enter first number : "))
+# b = int (input ("Enter the second number : "))
+# sum = a + b
+# print ("Sum of two numbers is : ", sum)
+#--------------------------------------------
+#program to take the side of a square from the user and print the area of the square
+# print ("Area of square : ")
+# side = float (input ("Enter the side of square : "))
+# area = side * side
+# print ("Area of square is : ", area)
+#---------------------------------------------
+#program to take two floating point numbers and print thier average
+print ("Average of two numbers : ")
+a = float (input ("Enter the first number :"))
+b = float (input ("Enter the second number :"))
+avg = (a + b) / 2   
+print ("Average of two numbers is : ", avg)
