5231a08aa9c6874d277ebbfadfcfb3907b67755b
QasimNaveed-stack <qsimnaveed303@gmail.com>
2026-07-06 20:17:32 +0500

    Add string, if-else, list, and tuple practice

---
+++ python_basics/lists&tupples.py
@@ -0,0 +1,79 @@
+#List :
+#   A built-in data structure in python which is used to store multiple items in a single variable.
+#   Lists are mutable, ordered and allows duplicate elements.    
+#   e.g. list1 = [1, 2, 3, 4, 5]
+# print ("List : ")
+# list1 = [1, 2, 3, 4, 5]
+# print (list1)
+# print ("First element of list1 is : ", list1[0])
+# print ("Last element of list1 is : ", list1[-1])
+# print ("Length of list1 is : ", len(list1))
+# print (type(list1)) # <class 'list'>
+# #   Lists can contain elements of different data types.
+# list2 = [1, "Hello", 3.14, True]
+# print ("List with different data types : ", list2)
+# # lists are mutable, we can change the value of an element in a list.
+# list1[0] = 10
+# print ("List after changing the first element : ", list1)
+# # List slicing
+# print ("List slicing : ", list1[1:4]) # [2, 3, 4]
+# # List methods
+# list1.append(6)
+# print ("List after appending an element : ", list1)
+# list1.remove(3)
+# print ("List after removing an element : ", list1)   
+# list2 = [7, 8, 9]
+# list2.insert(2, 3)
+# print ("List after inserting an element : ", list2) 
+# list2.sort()
+# print ("List after sorting : ", list2  )
+# list2.reverse()
+# print ("List after reversing : ", list2)
+# list2.pop(1)
+# print ("List after popping an element : ", list2)
+# # -----------------------------------------------
+# # Tuples :
+# #   A built-in data structure in python which is used to store multiple items in a single variable.
+# #   Tuples are immutable, ordered and allows duplicate elements.
+# tuple1 = (1, 2, 3, 4, 5)
+# print ("Tuple : ", tuple1)
+# print ("First element of tuple1 is : ", tuple1[0])
+# print ("Last element of tuple1 is : ", tuple1[-1])
+# print ("Length of tuple1 is : ", len(tuple1))
+# print (type(tuple1)) # <class 'tuple'>
+# # slicing of tuple
+# print ("Tuple slicing : ", tuple1[1:4]) # (2, 3, 4)
+# # tuple methods
+# tuple2 = (1, "Hello", 3.14, True)
+# print ("Tuple with different data types : ", tuple2)
+# tuple2.count(1) # 1
+# tuple2.index("Hello") # 1
+# # ------------------------------------------------
+# # practice programs on lists and tuples
+# # ------------------------------------------------
+# # Program to take input of three movie names from user and store them in a list.
+# print ("Enter three movie names : ")
+# movies = []
+# movie1 = input("Enter your first movie name : ")
+# movie2 = input("Enter your second movie name : ")  
+# movies.append(movie1)
+# movies.append(movie2)   
+# movies.append(input("Enter your third movie name : "))
+# print ("List of movies : ", movies) 
+# #  -------------------------------------------------
+# # Program to check if a list contains a palindrome of elements or not.
+# list3 = [1,2,3,4,1]
+# list4 = list3.copy()
+# list4.reverse()
+# if (list3 == list4):
+#     print ("List is a palindrome ")
+# else:
+#     print ("List is not a palindrome")    
+# -------------------------------------------------
+# Program to cont number of grade A in a marks tuple.
+marks = ("A", "B", "C", "A", "D", "A", "B")
+print ("Number of grade A in marks tuple is : ", marks.count("A"))
+# Now store this tuple  in a list and sort them from "A" to "D"
+marks_list = list(marks)#explict type casting
+marks_list.sort()
+print ("Sorted marks list : ", marks_list)
--- python_basics/python-basic.py
+++ python_basics/python-basic.py
@@ -32,8 +32,81 @@
 # print ("Area of square is : ", area)
 #---------------------------------------------
 #program to take two floating point numbers and print thier average
-print ("Average of two numbers : ")
-a = float (input ("Enter the first number :"))
-b = float (input ("Enter the second number :"))
-avg = (a + b) / 2   
-print ("Average of two numbers is : ", avg)
+# print ("Average of two numbers : ")
+# a = float (input ("Enter the first number :"))
+# b = float (input ("Enter the second number :"))
+# avg = (a + b) / 2   
+# print ("Average of two numbers is : ", avg)
+str = "Python is a programming language"
+str1 = 'Python is a programming language'
+str2 = """Python is a programming language"""
+# print (str)
+# print (str1)
+# print (str2)
+# #   \n is used to print the next string in new line
+# print ("Hello\nWorld")
+# # \t is used to print the next string after a tab space
+# print ("Hello\tWorld")  
+# # CONCATINATION OF STRINGS
+# str3 = str + str1
+# print (str3) # Python is a programming languagePython is a programming language
+# # length of string
+# print (len(str3)) # 56
+# print (len(str1)) # 34
+# str4 = str + " " + str1
+# print (str4) # Python is a programming language Python is a programming language
+# print (len(str4)) # 70
+# # indexing of string
+# print (str4[0]) # P
+# print (str4[1]) # y
+# print (str4[6]) # space 
+# # Slicing of string
+# # str4[start:end] # start index is inclusive and end index is exclusive
+# print (str4[0:6]) # Python
+# print (str4[7:17]) # is a programming
+# print (str4[0:len(str4)]) # Python is a programming language Python is a programming language
+# print (str4[0:]) # Python is a programming language Python is a programming language
+# print (str4[:len(str4)]) # Python is a programming language Python is a programming
+# # in python we can do negative indexing also
+# print (str4[-1]) # g
+# print (str4[-2]) # n
+# # slicing with negative indexing
+# print (str4[-10:-1]) # language
+# # string methods
+# str5 = "python is a programming language"
+# print (str5.upper()) # PYTHON IS A PROGRAMMING LANGUAGE
+# print (str5.lower()) # python is a programming language
+# print (str5.capitalize()) # Python is a programming language
+# print (str5.title()) # Python Is A Programming Language
+# #string comparison
+# print (str5 == "python is a programming language") # True   
+# print (str5.startswith("python")) # True
+# print (str5.endswith("language")) # True
+# #string searching
+# print (str5.find("programming")) # 14   
+# # string replacement
+# str6 = str5.replace("programming", "coding")
+# print (str6) # python is a coding language
+# # string split
+# str7 = str5.split(" ")  
+# print (str7) # ['python', 'is', 'a', 'programming', 'language']
+# # string join
+# str8 = "-".join(str7)
+# print (str8) # python-is-a-programming-language
+# # find function returns the index of the first occurrence of the substring, if found. If not found, it returns -1.
+# print (str5.find("python")) # 0
+# print (str5.find("Java")) # -1
+# print (str5.find("is")) # 7
+# # count function returns the number of occurrences of the substring in the string.
+# print (str5.count("is")) # 1    
+# print (str5.count("a")) # 4
+
+# Some programs to practice string methods
+# 1. Program to take input user's first name and print its length
+print ("Hello, welcome to string methods practice programs")
+first_name = input("Enter your first name : ")
+print ("Length of your first name is : ", len(first_name))
+
+# 2. program to find the occurrence of $ in the a string
+str9 = input("Enter a string : ")
+print ("Occurrence of $ in the string is : ", str9.find("$"))
